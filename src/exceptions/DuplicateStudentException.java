f714edff7bc68ecf547438d50cde169577d60d39
BirukG09 <birukgebre277@gmail.com>
2026-06-23 02:26:34 +0300

    Add duplicateId field and getter to exception

--- src/exceptions/DuplicateStudentException.java
+++ src/exceptions/DuplicateStudentException.java
@@ -1,7 +1,12 @@
-package exceptions;
-
 public class DuplicateStudentException extends Exception {
+    private final String duplicateId;  
+
     public DuplicateStudentException(String studentId) {
         super("A student with ID " + studentId + " is already registered");
+        this.duplicateId = studentId;  
+    }
+
+    public String getDuplicateId() {   
+        return duplicateId;
     }
 }
