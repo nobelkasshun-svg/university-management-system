05fe62d076241d679d063fe35fa9da23ea5fa740
BirukG09 <birukgebre277@gmail.com>
2026-06-23 02:28:22 +0300

    Enhance InvalidGradeException with additional constructor

--- src/exceptions/InvalidGradeException.java
+++ src/exceptions/InvalidGradeException.java
@@ -1,7 +1,15 @@
-package exceptions;
-
 public class InvalidGradeException extends Exception {
+    private final double invalidGrade;  
+
     public InvalidGradeException(String message) {
         super(message);
+        this.invalidGrade = -1;  
     }
+
+    public InvalidGradeException(double grade) {   
+        super("Invalid grade: " + grade + ". Must be between 0 and 100.");
+        this.invalidGrade = grade;
+    }
+
+    public double getInvalidGrade() { return invalidGrade; }  
 }
