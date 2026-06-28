885770e067c5ca001ae89fdc12686f3947452d85
BirukG09 <birukgebre277@gmail.com>
2026-06-28 23:56:46 +0300

    Update InvalidNameException.java

--- src/exceptions/InvalidNameException.java
+++ src/exceptions/InvalidNameException.java
@@ -1,7 +1,6 @@
 package exceptions;
 
 public class InvalidNameException extends Exception {
-
     public InvalidNameException(String invalidName, String reason) {
         super("Invalid name '" + invalidName + "': " + reason);
     }
