4b48dca848b88b5a21f3b9e6ecd89b0b51d84617
BirukG09 <birukgebre277@gmail.com>
2026-06-28 23:58:08 +0300

    Refactor InvalidGradeException constructor and formatting

--- src/exceptions/InvalidGradeException.java
+++ src/exceptions/InvalidGradeException.java
@@ -1,15 +1,17 @@
+package exceptions;
+
 public class InvalidGradeException extends Exception {
-    private final double invalidGrade;  
+    private final double invalidGrade;
 
     public InvalidGradeException(String message) {
         super(message);
-        this.invalidGrade = -1;  
+        this.invalidGrade = -1;
     }
 
-    public InvalidGradeException(double grade) {   
+    public InvalidGradeException(double grade) {
         super("Invalid grade: " + grade + ". Must be between 0 and 100.");
         this.invalidGrade = grade;
     }
 
-    public double getInvalidGrade() { return invalidGrade; }  
+    public double getInvalidGrade() { return invalidGrade; }
 }
